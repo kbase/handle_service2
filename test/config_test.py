0cf06ea0fc57bb8f5de7f9f59b0ef5005b068718
Sijie <sijiex@lbl.gov>
2024-05-30 00:19:11 -0700

    fix linely

--- test/config_test.py
+++ test/config_test.py
@@ -4,7 +4,8 @@ import unittest
 
 from pathlib import Path
 
-import mango_util
+import mongo_util
+
 
 class ConfigTest(unittest.TestCase):
 
@@ -24,11 +25,11 @@ class ConfigTest(unittest.TestCase):
         """
         try:
             cfg_path = self._create_temp_file(data_string)
-            default_path = os.environ[mango_util.TEST_CONFIG_ENV_PATH]
-            os.environ[mango_util.TEST_CONFIG_ENV_PATH] = cfg_path
+            default_path = os.environ[mongo_util.TEST_CONFIG_ENV_PATH]
+            os.environ[mongo_util.TEST_CONFIG_ENV_PATH] = cfg_path
             mongo_cfg, deploy_cfg = mongo_util.get_config()
         finally:
-            os.environ[mango_util.TEST_CONFIG_ENV_PATH] = default_path
+            os.environ[mongo_util.TEST_CONFIG_ENV_PATH] = default_path
 
         # check mongo config
         self.assertEqual(mongo_cfg.mongo_exe, Path("mongod"))
@@ -37,16 +38,21 @@ class ConfigTest(unittest.TestCase):
         self.assertEqual(mongo_cfg.delete_temp_dir, True)
 
         # check deploy config
-        self.assertEqual(deploy_cfg["auth-service-url"], 'https://ci.kbase.us/services/auth/api/legacy/KBase/Sessions/Login')
-        self.assertEqual(deploy_cfg["auth-url"], 'https://ci.kbase.us/services/auth')
-        self.assertEqual(deploy_cfg["shock-url"], 'https://ci.kbase.us/services/shock-api')
-        self.assertEqual(deploy_cfg["admin-token"], '998')
-        self.assertEqual(deploy_cfg["test-token"], '998')
-        self.assertEqual(deploy_cfg["admin-roles"], 'HANDLE_ADMIN, KBASE_ADMIN')
-        self.assertEqual(deploy_cfg["namespace"], 'KBH')
-        self.assertEqual(deploy_cfg["mongo-database"], 'handle_db')
-        self.assertEqual(deploy_cfg["mongo-user"], '')
-        self.assertEqual(deploy_cfg["mongo-password"], '')
+        self.assertEqual(
+            deploy_cfg["auth-service-url"],
+            "https://ci.kbase.us/services/auth/api/legacy/KBase/Sessions/Login",
+        )
+        self.assertEqual(deploy_cfg["auth-url"], "https://ci.kbase.us/services/auth")
+        self.assertEqual(
+            deploy_cfg["shock-url"], "https://ci.kbase.us/services/shock-api"
+        )
+        self.assertEqual(deploy_cfg["admin-token"], "998")
+        self.assertEqual(deploy_cfg["test-token"], "998")
+        self.assertEqual(deploy_cfg["admin-roles"], "HANDLE_ADMIN, KBASE_ADMIN")
+        self.assertEqual(deploy_cfg["namespace"], "KBH")
+        self.assertEqual(deploy_cfg["mongo-database"], "handle_db")
+        self.assertEqual(deploy_cfg["mongo-user"], "")
+        self.assertEqual(deploy_cfg["mongo-password"], "")
         self.assertEqual(deploy_cfg["mongo-retrywrites"], False)
 
         # remove temp file
@@ -72,11 +78,11 @@ class ConfigTest(unittest.TestCase):
         """
         try:
             cfg_path = self._create_temp_file(data_string)
-            default_path = os.environ[mango_util.TEST_CONFIG_ENV_PATH]
-            os.environ[mango_util.TEST_CONFIG_ENV_PATH] = cfg_path
+            default_path = os.environ[mongo_util.TEST_CONFIG_ENV_PATH]
+            os.environ[mongo_util.TEST_CONFIG_ENV_PATH] = cfg_path
             mongo_cfg, deploy_cfg = mongo_util.get_config()
         finally:
-            os.environ[mango_util.TEST_CONFIG_ENV_PATH] = default_path
+            os.environ[mongo_util.TEST_CONFIG_ENV_PATH] = default_path
 
         # check mongo config
         self.assertEqual(mongo_cfg.mongo_exe, Path("mongod"))
@@ -85,16 +91,21 @@ class ConfigTest(unittest.TestCase):
         self.assertEqual(mongo_cfg.delete_temp_dir, False)
 
         # check deploy config
-        self.assertEqual(deploy_cfg["auth-service-url"], 'https://ci.kbase.us/services/auth/api/legacy/KBase/Sessions/Login')
-        self.assertEqual(deploy_cfg["auth-url"], 'https://ci.kbase.us/services/auth')
-        self.assertEqual(deploy_cfg["shock-url"], 'https://ci.kbase.us/services/shock-api')
-        self.assertEqual(deploy_cfg["admin-token"], '998')
-        self.assertEqual(deploy_cfg["test-token"], '998')
-        self.assertEqual(deploy_cfg["admin-roles"], 'HANDLE_ADMIN, KBASE_ADMIN')
-        self.assertEqual(deploy_cfg["namespace"], 'KBH')
-        self.assertEqual(deploy_cfg["mongo-database"], 'handle_db')
-        self.assertEqual(deploy_cfg["mongo-user"], 'mongouser')
-        self.assertEqual(deploy_cfg["mongo-password"], 'mongopassword')
+        self.assertEqual(
+            deploy_cfg["auth-service-url"],
+            "https://ci.kbase.us/services/auth/api/legacy/KBase/Sessions/Login",
+        )
+        self.assertEqual(deploy_cfg["auth-url"], "https://ci.kbase.us/services/auth")
+        self.assertEqual(
+            deploy_cfg["shock-url"], "https://ci.kbase.us/services/shock-api"
+        )
+        self.assertEqual(deploy_cfg["admin-token"], "998")
+        self.assertEqual(deploy_cfg["test-token"], "998")
+        self.assertEqual(deploy_cfg["admin-roles"], "HANDLE_ADMIN, KBASE_ADMIN")
+        self.assertEqual(deploy_cfg["namespace"], "KBH")
+        self.assertEqual(deploy_cfg["mongo-database"], "handle_db")
+        self.assertEqual(deploy_cfg["mongo-user"], "mongouser")
+        self.assertEqual(deploy_cfg["mongo-password"], "mongopassword")
         self.assertEqual(deploy_cfg["mongo-retrywrites"], True)
 
         # remove temp file
@@ -102,11 +113,10 @@ class ConfigTest(unittest.TestCase):
 
     def _create_temp_file(data_string):
         try:
-            with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
+            with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_file:
                 temp_file.write(data_string)
                 file_path = temp_file.name
             return file_path
 
         except Exception as e:
             raise ValueError("An error occurred:") from e
-
