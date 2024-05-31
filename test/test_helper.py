ddf6f55edde4f1366e725bac6e8b19b4d7a1d1cf
Sijie <sijiex@lbl.gov>
2024-05-31 10:26:50 -0700

    remove test/config_test.py

--- test/test_helper.py
+++ test/test_helper.py
@@ -1,6 +1,3 @@
-import tempfile
-
-
 def get_hid_counter(hid_counter_collection):
     """
     get current handle id counter
@@ -10,14 +7,3 @@ def get_hid_counter(hid_counter_collection):
     if counter is not None:
         return counter.get("hid_counter")
     return 0
-
-
-def create_temp_file(data_string):
-    try:
-        with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_file:
-            temp_file.write(data_string)
-            file_path = temp_file.name
-        return file_path
-
-    except Exception as e:
-        raise ValueError("An error occurred:") from e
